60c5ec73d7c36ec3fd25ee6ff313322f746f030a
daniel329paul <daniel.paul@datasemantics.in>
2021-07-27 20:04:59 +0530

    blob upload

--- src/components/FileUploadForm.js
+++ src/components/FileUploadForm.js
@@ -3,29 +3,28 @@ import React from 'react'
 export function FileUploadForm() {
     
     const  submitHander = async ()=>{
-        let user_name = document.querySelector('#user_name').value.trim();
-        let user_email = document.querySelector('#user_email').value.trim();
-        if(user_email != '' && user_name !=''){
+        var formData = new FormData(document.querySelector('#form'))
+        console.log(formData)
+        if(formData){
             // call trigger
-            let data = {'user_name': user_name, 'user_email': user_email};
-            let url = 'https://fnds20334.azurewebsites.net/api/trigger1'
+            let url = 'https://fnds20334b.azurewebsites.net/api/HttpTrigger1'
             const response = await fetch(url, {
                 method: 'POST', // *GET, POST, PUT, DELETE, etc.
                 mode: 'cors', // no-cors, *cors, same-origin
                 headers: {
                   'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(data) // body data type must match "Content-Type" header
+                body: formData // body data type must match "Content-Type" header
               });
               console.log(response.json());
               return response.json();
         }
     };
     return (
-        <div>
-            <input type="file" id="az_file_input"/>
-            <button onClick={submitHander}>upload</button>
-        </div>
+        <form id="form" action="https://fnds20334b.azurewebsites.net/api/HttpTrigger1" method="POST" encType="multipart/form-data">
+            <input type="file" id="files" name="files"/>
+            <input type="submit" />
+        </form>
     )
 }
 
